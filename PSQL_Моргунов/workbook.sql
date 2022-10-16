6b603faa0730580281e37de2682d445013780670
AlexVechkan <108777771+AlexVachkan@users.noreply.github.com>
2022-10-16 22:01:48 +0500

    Add files via upload

--- PSQL_Моргунов/workbook.sql
+++ PSQL_Моргунов/workbook.sql
@@ -1,4 +1,5 @@
-create table public.aircraft
+-- \dn вывод схем бд
+create table public.aircrafts
 (aircraft_code char(3) not null,
 model text not null,
 range integer not null,
@@ -6,7 +7,7 @@ check (range > 0),
 primary key (aircraft_code)
 );
 
-insert into public.aircraft(aircraft_code,model,range)
+insert into public.aircrafts(aircraft_code,model,range)
 values  ('773',	'Боинг 777-300',	11100),
 		('763',	'Боинг 767-300',	7900),
 		('SU9',	'Сухой Суперджет-100',	3000),
@@ -17,14 +18,34 @@ values  ('773',	'Боинг 777-300',	11100),
 		('CN1',	'Сессна 208 Караван',	1200),
 		('CR2',	'Бомбардье CRJ-200',	2700);
 
-
-update public.aircraft
+update public.aircrafts
 set range = 3500
 where aircraft_code='SU9';
 
 delete from public.aircraft
 where aircraft_code='CN1';
 
-select * from public.aircraft;
+select * from public.aircrafts;
 
-drop table aircraft
+drop table aircraft;
+-----------------------------------
+-- создаю таблицу seats. Это ссылающаяся таблица на aircrafts(ссылочная).
+create table public.seats
+(
+	aircraft_code char(3) not null, -- обязательно три символа
+	seat_no varchar(4) not null, -- не более 4х символов
+	fare_condition varchar(10) not null,
+	check 
+	(fare_condition in ('economy', 'comfort', 'bisiness')), -- ограничение по трем словам
+	primary key (aircraft_code, seat_no), -- составной первичный ключ
+	foreign key (aircraft_code) -- вынешний ключ 
+	  references public.aircrafts (aircraft_code) -- указываю на какая таблица ссылочная  и атрибут в жтой таблице
+	  on delete cascade -- задаю каскадное удаление
+);
+
+-- проверим работу внешнего ключа
+insert into public.seats values ('123', '1A', 'bisiness');
+-- Ключ (aircraft_code)=(123) отсутствует в таблице "aircrafts".
+
+select * from bookings.seats
+
